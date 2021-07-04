86899e5761b90fceaa71b6a6da47404c202f2e94
anjalkhadka <Anjalkhadka>
2021-07-04 14:11:21 +0545

    lab4

--- qn3.py
+++ qn3.py
@@ -1,1 +1,6 @@
 '''Write a Python program to guess a number between 1 to 9.'''
+import random
+a,b=random.randint(1,9),0
+while a!=b:
+    b=int(input("enter 1to9 "))
+print("well guessed")
---
+++ qn5.py
@@ -0,0 +1,3 @@
+'''Write a Python program that accepts a word from the user and reverse it.'''
+word=str(input("enter  word : "))
+print(word[::-1])
